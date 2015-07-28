ff37139fbd87e475972981bb6cccf79e81faa67d
Karan Hiremath <karanhir@seas.upenn.edu>
2015-07-28 00:34:24 -0700

    phaxio -> s3

--- index.js
+++ index.js
@@ -10,16 +10,18 @@ const APP_PORT = envvar.number('APP_PORT', 8080);
 
 const app = express();
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 app.get('/', (req,res) => {
   res.render('')
 });
 
-app.post('/fax/webhook', (req, res) => {
-  console.log('\n\n\n\n\n', req);
-  handle.receiveFax(req.body.fax);
-  res.sendStatus(200);
+app.post('/fax/webhook', (req, res, next) => {
+  handle.receiveFax(req)
+        .then(() => {
+          res.sendStatus(200);
+        })
+        .catch(res.send);
 });
 
 app.listen(APP_PORT, () => {
